34ed4729237d403fb4d9fc0ec8ddc9c499e765d9
Tavinou <gustavoh.roepke@gmail.com>
2025-02-18 15:39:19 -0300

    colisao de bolas

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
+
 import pygame
+from random import randint
+import math
 class Bola:
     def __init__(self,x,y,raio,cor,velocidade_x,velocidade_y,tela):
         self.x = x
@@ -8,25 +11,35 @@ class Bola:
         self.velocidade_x = velocidade_x
         self.velocidade_y = velocidade_y
         self.tela = tela
+    def mudarcor(self):
+        self.cor = (randint(0,255),randint(0,255),randint(0,255))
     def mover(self):
         self.x += self.velocidade_x
         self.y += self.velocidade_y
 
         # Impede que a bola ultrapasse a tela
         if self.x - self.raio <= 0 or self.x + self.raio >= 800:
-            self.velocidade_x *= -1  # Rebater nas bordas horizontais
+            self.velocidade_x *= -1  # Rebater nas bordas horizontai
+            self.mudarcor()
         if self.y - self.raio <= 0 or self.y + self.raio >= 600:
             self.velocidade_y *= -1  # Rebater nas bordas verticais
-    
-
+            self.mudarcor()
+    def verifica_colisao(self,outra_bola):
+        distancia = math.sqrt((outra_bola.x - self.x) ** 2 + (outra_bola.y - self.y) ** 2)
+        if distancia <= self.raio + outra_bola.raio:
+            self.velocidade_x *= -1
+            self.velocidade_y *= -1 
+            outra_bola.velocidade_x*= -1
+            outra_bola.velocidade_y *= -1
+            self.mudarcor()
+            outra_bola.mudarcor()
     def desenhar(self):
         pygame.draw.circle(self.tela,self.cor,(self.x,self.y),self.raio)
-pygame.init()
-largura, altura = 800, 600
-tela = pygame.display.set_mode((largura, altura))
-clock = pygame.time.Clock()
 
 
+
+
+    
     
 
     
--- simulacao.py
+++ simulacao.py
@@ -1,23 +1,40 @@
-import classes
+from classes import Bola
 import pygame
-
+from random import randint
+def mudarcor():
+        return (randint(0,255),randint(0,255),randint(0,255))
 pygame.init()
 largura, altura = 800, 600
 tela = pygame.display.set_mode((largura, altura))
 clock = pygame.time.Clock()
+#listabolas = []
+#for i in range(0,2):
+    #listabolas.append(Bola(randint(40,770),randint(40,560),30,mudarcor(),5,5,tela))
 
-bola = classes.Bola(250, 300, 30, (255,255,255), 5, 0, tela)
+bola = Bola(250, 300, 20, (255, 255, 0), 5, 5, tela)
+bola1 = Bola(80, 200, 20, (255, 255, 0), 5, 5, tela)
+bola2 = Bola(50,50, 20,(255, 255, 0), 5, 5, tela)
+bola3 = Bola(159,50, 20,(255, 255, 0), 5, 5, tela)
 rodando = True
 while rodando:
     tela.fill((0, 0, 0))  
-
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
-
-    bola.mover()  
+    bola.mover()
+    bola1.mover()
+    bola2.mover()
+    bola3.mover()
+    bola.verifica_colisao(bola1)
+    bola.verifica_colisao(bola2)
+    bola.verifica_colisao(bola3)
+    bola1.verifica_colisao(bola3)
+    bola1.verifica_colisao(bola2)
+    bola2.verifica_colisao(bola3)
     bola.desenhar()
-
+    bola1.desenhar()
+    bola2.desenhar()
+    bola3.desenhar()
     pygame.display.flip()  
     clock.tick(60)  
 

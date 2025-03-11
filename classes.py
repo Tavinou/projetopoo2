7c69968b195f0ad5c84c2e251de01915475641b4
Tavinou <gustavoh.roepke@gmail.com>
2025-03-11 04:01:32 +0000

    gerador de bolas

--- classes.py
+++ classes.py
@@ -18,30 +18,41 @@ class Bola:
         self.velocidade_y += 0.5 #aplica gravidade para fazer uma parabola
         self.y += self.velocidade_y
         if self.x - self.raio <= 0 or self.x + self.raio >= 800:
+            if self.x - self.raio<0: # n deixa entrar na parede
+                self.x = self.raio # n deixa entrar na parede
+            if self.x + self.raio>=800:# n deixa entrar na parede
+                self.x= 800-self.raio# n deixa entrar na parede
             self.velocidade_x *= -1  # Rebate nas bordas horizontai
-            self.mudarcor()
+            #self.mudarcor()
         if self.y - self.raio <= 0:
             self.velocidade_y *= -1  # Rebate no teto
-            self.mudarcor()
-        if self.y + self.raio>=600: #se bater no chao
+            #self.mudarcor()
+        if self.y + self.raio>600: #se bater no chao
             self.y=600 - self.raio #para bola n entra em baixo
-            self.velocidade_y = -20 #reseta altura/pulo sem perder velocidade
-            self.mudarcor()
+            self.velocidade_y *= -1 #reseta altura/pulo sem perder velocidade
+            #self.mudarcor()
     def verifica_colisao(self,outra_bola):
         distancia = math.sqrt((outra_bola.x - self.x) ** 2 + (outra_bola.y - self.y) ** 2)
         if distancia <= self.raio + outra_bola.raio:
+            if self.x>outra_bola.x: # n deixa entra uma na outra
+                self.x+=2 
+                outra_bola.x-=2
+            if self.y>outra_bola.y: # n deixa entra uma na outra
+                self.y+=2
+                outra_bola.y-=2 
             self.velocidade_x, outra_bola.velocidade_x = outra_bola.velocidade_x, self.velocidade_x
             self.velocidade_y, outra_bola.velocidade_y = outra_bola.velocidade_y, self.velocidade_y
+            self.velocidade_y+=0.1
             #troca de energia 
-            self.mudarcor()
-            outra_bola.mudarcor()
+            #self.mudarcor()
+            #outra_bola.mudarcor()
     def desenhar(self):
         pygame.draw.circle(self.tela,self.cor,(self.x,self.y),self.raio)
 
 
 
 
-    
+
     
 
     
--- simulacao.py
+++ simulacao.py
@@ -4,24 +4,30 @@ from random import randint
 pygame.init()
 largura, altura = 800, 600
 tela = pygame.display.set_mode((largura, altura))
+def mudarcor():
+        return randint(0,255),randint(0,255),randint(0,255)
 clock = pygame.time.Clock()
-#listabolas = []
-#for i in range(0,2):
-    #listabolas.append(Bola(randint(40,770),randint(40,560),30,mudarcor(),5,5,tela))
+listabolas = [] #lista de bola
+#gerador de bolas
+for i in range(0,20): #n botar 250
+    listabolas.append(Bola(randint(40,770),randint(40,560),randint(5,20),mudarcor(),randint(-5,5),randint(-5,5),tela))
+
 rodando = True
-bola = Bola(250, 300, 20, (255, 255, 0), 5, 5, tela)
-bola1 = Bola(704, 60, 20, (255, 255, 0), 5, 5, tela)
 while rodando:
     tela.fill((0, 0, 0))  
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
-    pygame.draw.circle(tela, (255, 0, 0), (400, 300), 90,2)
-    bola.mover()
-    bola1.mover()
-    bola.verifica_colisao(bola1)
-    bola1.desenhar()   
-    bola.desenhar()
+    #pygame.draw.circle(tela, (255, 0, 0), (400, 300), 90,2)
+    #chama todos os metodos das bolas
+    for b in listabolas: # percore a lista de bolas
+        b.mover() #chama metedo mover
+        b.desenhar() #ndesenha
+        for i in listabolas: #percore dnv
+            if b==i: #se for o mesmo objeto n faz nd
+                None
+            else: # diferente passa o objeto para verificar a colisao
+                 b.verifica_colisao(i)
     pygame.display.flip()  
     clock.tick(60)  
 
